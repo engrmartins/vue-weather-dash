b97adacb07c30cb7dfc3e268cbb636900b5a84de
Enobong Martins <enobong.martins@e4email.net>
2019-10-04 01:35:56 +0100

    initial commit

---
+++ Desktop/side/climatedash/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+import MainSection from ''
+import ForecastChart from '/components/ForecastChart.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MainSection,
+    ForecastChart    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
